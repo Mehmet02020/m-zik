21edcd7f75009bd0d962a7510735769d284df975
Mehmet02020 <106279374+Mehmet02020@users.noreply.github.com>
2022-06-23 02:55:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ load_dotenv()
 que = {}
 admins = {}
 
-SESSION_NAME = getenv("SESSION_NAME", "session")
-BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME", "Black") 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
-BOT_USERNAME = getenv("BOT_USERNAME", "blackmuzikbot")
+SESSION_NAME = getenv("SESSION_NAME", "1BJWap1wBu6zIEZRtoTX7-uFpTUnJhBZkFkDEgXVNH8TSLbLOYKlfVmMMF-GMDlGCCtWwt6quM19tye2bq6L8N3PZRFvhutf_FGkEWNJzevCqijdGLiUGRifmaB7Reuyxnfe7vS26ILobjySLor5Z8KCDtCI-qHMV2tuaTgGBUX6F3eQhMNM-Cj7zD2NgNVgOaJIWSMbJsRrwxWWgRIcStUnVh257PnUwZ09gR0XH6ZsqzljKALoCjHW3hjmi-BYI6pDYvrOxIbsp9XYFMGlXpgEE2Nn7N4c7ecSkpcGHbbA1c2_LkJh-sSLUZRDjzouxqeuOLefI92wzEdGoW2xhX6Cq4wACIqY")
+BOT_TOKEN = getenv("5343798470:AAFfBax1Qhib7ZFPebSwaKMzVmC3aGj21zc")
+BOT_NAME = getenv("black", "Black") 
+API_ID = int(getenv("10318886"))
+API_HASH = getenv("8b5e314a1655f64e19bf46083976114f")
+BOT_USERNAME = getenv("blackmuzikbot", "blackmuzikbot")
 PMPERMIT = getenv("PMPERMIT", "ENABLE")
-DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
+DURATION_LIMIT = int(getenv("55", "7"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+SUDO_USERS = list(map(int, getenv("5306202921").split()))
